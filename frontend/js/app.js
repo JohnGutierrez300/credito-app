a56eda8c6437c5db439a2b9b8195f88dd08585f8
JohnGutierrez300 <johnjaviergutierrez1@gmail.com>
2026-06-03 23:07:22 -0500

    actualizacion pwa y api

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -114,3 +114,13 @@ if (btnSync) {
 document.addEventListener("DOMContentLoaded", () => {
     showScreen("clientes");
 });
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js")
+            .then(() => console.log("SW registrado"))
+            .catch(err => console.log("SW error", err));
+    });
+}
